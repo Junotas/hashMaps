02a996a65340fb43b020dfcc6798d008f1368efc
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-05 13:13:02 +0200

    commented out the old one and have started following along on broCodes version

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,36 @@ public class Main {
     public static void main(String[] args) {
 
 
+        HashMap<String,String> countries = new HashMap<>();
+
+
+        //key and their value, here it's country and capital
+        countries.put("USA","Washington DC");
+        countries.put("India","New Delhi");
+        countries.put("Russia","Moscow");
+        countries.put("China","Beijing");
+
+
+        //countries.remove("USA");
+        System.out.println(countries);
+
+
+
+
+/*
         HashMap<String,Integer> drinksCalories = new HashMap<>();
+        //key and their value, here it's a drink and calories of that drink
 
         drinksCalories.put("Redbull", 100);
         drinksCalories.put("Coca Cola", 130);
         drinksCalories.put("Fanta", 115);
         drinksCalories.put("Powerking", 110);
 
-        System.out.println(drinksCalories.get("Redbull"));
+        //System.out.println(drinksCalories.get("Redbull"));
 
+
+        //System.out.println();
+*/
 
     }
 }
