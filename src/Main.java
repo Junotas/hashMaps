9154a8e1c49153c76ba41fcb36897400505636b3
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-05 13:25:48 +0200

    tried out all his method they're saved as comments

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,26 @@ public class Main {
 
 
         //key and their value, here it's country and capital
-        countries.put("USA","Washington DC");
+        countries.put("USA    ","Washington DC");
         countries.put("India","New Delhi");
         countries.put("Russia","Moscow");
         countries.put("China","Beijing");
 
-
+        // System.out.println(countries.size());
         //countries.remove("USA");
-        System.out.println(countries);
+       // System.out.println(countries.get("Russia"));
+        /*countries.clear();
+        System.out.println(countries);*/
+        //System.out.println(countries);
+       /* countries.replace("USA","Detroit");
+        System.out.println(countries);*/
+        /*System.out.println(countries.containsKey("England"));
+        System.out.println(countries.containsValue("Beijing"));*/
+
+        for (String i : countries.keySet()) {
+            System.out.print(i+"\t"+ "= ");
+            System.out.println(countries.get(i));
+        }
 
 
 
