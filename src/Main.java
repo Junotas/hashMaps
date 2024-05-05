c915f34c5fc373fbe395964ba8342f129ac1f147
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-05 12:59:40 +0200

    gets redbulls made up calories

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
 
+        HashMap<String,Integer> drinksCalories = new HashMap<>();
+
+        drinksCalories.put("Redbull", 100);
+        drinksCalories.put("Coca Cola", 130);
+        drinksCalories.put("Fanta", 115);
+        drinksCalories.put("Powerking", 110);
+
+        System.out.println(drinksCalories.get("Redbull"));
 
 
     }
